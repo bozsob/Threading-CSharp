f227d537f741836adf425a3f0add3f21481ff67b
bozsob <bozso.beatrix@gmail.com>
2017-07-13 12:51:18 +0200

    using Mutex to create single-instance app

---
+++ SingleInstance/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SingleInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const string MutexName = "RUNMEONCE";
+
+            try
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException) { }
+
+            if (oneMutex == null)
+            {
+                oneMutex = new Mutex(true, MutexName);
+            }
+            else
+            {
+                // Close the mutex and exit the application
+                // because we can only have one instance
+                oneMutex.Close();
+                return;
+            }
+
+            Console.WriteLine("Our Application");
+            Console.ReadKey();
+        }
+    }
+}
