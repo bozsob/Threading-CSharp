61523d409e6b21f7c5c55fd45c94e8cdc643887e
bozsob <bozso.beatrix@gmail.com>
2017-07-13 12:40:00 +0200

    Create multiple threads

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Threading
 {
@@ -10,6 +11,33 @@ namespace Threading
     {
         static void Main(string[] args)
         {
+            ThreadStart starter = new ThreadStart(Counting);
+            Thread first = new Thread(starter);
+            Thread second = new Thread(starter);
+
+            first.Start();
+            second.Start();
+
+            first.Join();
+            second.Join();
+
+            AppDomain currAD = AppDomain.CurrentDomain;
+            Console.WriteLine(currAD.FriendlyName);
+            
+            Console.WriteLine("Szál-Id: {0}", Thread.CurrentThread.ManagedThreadId);
+            
+            Console.ReadKey();
+
+        }
+
+        static void Counting()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine("Count: {0} and the Thread: {1}", 
+                    i, Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(10);
+            }
         }
     }
 }
